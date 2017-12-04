3e521aa3387bac9162881c264ebaf75c5d4ec4ad
eamon <eamonmurphy@outlook.com>
2017-12-04 12:22:19 -0800

    Updates - Removed Skrollr

---
+++ assets/js/header.js
@@ -0,0 +1,29 @@
+var scrollpos = window.scrollY;
+var header = document.getElementById("header");
+var contactbutton = document.getElementById("contactbutton");
+
+function add_class_on_scroll() {
+    header.classList.add("header-color");
+}
+function remove_class_on_scroll() {
+    header.classList.remove("header-color");
+}
+function button_add() {
+  contactbutton.classList.add("button-outline");
+}
+function button_remove() {
+  contactbutton.classList.remove("button-outline");
+};
+
+window.addEventListener('scroll', function(){
+    scrollpos = window.scrollY;
+    if(scrollpos > 150){
+        add_class_on_scroll();
+        button_add();
+    }
+    else {
+        remove_class_on_scroll();
+        button_remove();
+    }
+    console.log(scrollpos);
+});
